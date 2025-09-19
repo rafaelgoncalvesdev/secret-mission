4cbe9340d3259efe11e9e75ba8793cec513c49aa
rafaeldev <rafaelgm.00@gmail.com>
2025-09-19 18:48:07 -0300

    project boilerplate

---
+++ src/main/java/com/secretmission/app/SecretMissionApplication.java
@@ -0,0 +1,13 @@
+package com.secretmission.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretMissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecretMissionApplication.class, args);
+	}
+
+}
